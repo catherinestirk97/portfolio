e26a0a87f2bd2f434775a9c3e402d92363e398ae
catherinestirk97 <53531093+catherinestirk97@users.noreply.github.com>
2020-10-22 15:12:51 +0100

    add scroll buttons and js

---
+++ app.js
@@ -0,0 +1,20 @@
+function scrollToAbout() {
+    document.querySelector('.about').scrollIntoView({ 
+        behavior: 'smooth',
+        block: 'start'
+      });
+}
+
+function scrollToProjects() {
+    document.querySelector('.projects').scrollIntoView({ 
+        behavior: 'smooth',
+        block: 'start'
+      });
+}
+
+function scrollToContact() {
+    document.querySelector('.contact').scrollIntoView({ 
+        behavior: 'smooth',
+        block: 'start'
+      });
+}
